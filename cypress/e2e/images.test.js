9d266c5e317a0fbb454c7eefd18372765d03ea84
mariobrusarosco <mariobrusarosco@hotmail.com>
2018-12-27 19:43:23 -0200

    Fixes the 'Images Test' and sets the webpack test script in travis config

--- cypress/e2e/images.test.js
+++ cypress/e2e/images.test.js
@@ -1,10 +1,12 @@
 describe('Test for support of Images', () => {
     it('Must find an image imported via JS "import" declaration', () => {
-        cy.get('#image-via-js')
+        cy.visit('/')
+            .get('#image-via-js')
     })
 
     it('Must find an image import as a background-image url() function', () => {
-        cy.get('#image-via-css')
-        .should('have.css', 'background-image')
+        cy.visit('/')
+            get('#image-via-css')
+            .should('have.css', 'background-image')
     })
 })
