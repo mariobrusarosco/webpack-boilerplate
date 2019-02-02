e170af60f1dd03446cf4b70379df8b9a0f7c2893
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-02-02 12:04:32 -0200

    Feature: Generates .gzip and Brotli

--- webpack/plugins/production/index.js
+++ webpack/plugins/production/index.js
@@ -1,13 +1,21 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
 
 const productionPlugins = [
   new MiniCssExtractPlugin({
     filename: '[name]-[contenthash].css',
     chunkFilename: '[id]-[contenthash].css'
   }),
-  new OptimizeCSSAssetsPlugin({})
+  new OptimizeCSSAssetsPlugin({}),
   // new webpack.HashedModuleIdsPlugin() // Apparently, webpack is not requiring this plugin anymore. It may be doing the job without it!
+  new CompressionPlugin({
+    exclude: /node_modules/,
+    cache: true,
+    algorithm: 'gzip'
+  }),
+  new BrotliPlugin()
 ]
 
 module.exports = productionPlugins
