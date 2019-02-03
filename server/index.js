535e44d1bd332d0626c5e69fd2fd3a4420fe1a68
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-02-03 21:31:29 -0200

    Changing dyno from web to worker

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`server listening at ${PORT}`)
 })
