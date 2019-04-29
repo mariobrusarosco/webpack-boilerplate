8ce1b731c992387f12bd62ca96b38cddf5cbf211
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-04-28 21:24:13 -0300

    Fixing Cypress Version

--- cypress/e2e/dynamic-code-splitting.test.js
+++ cypress/e2e/dynamic-code-splitting.test.js
@@ -1,10 +1,9 @@
-describe('', () => {
-	it('', () => {
-		cy.visit('/')
+describe('Testing Code Splitting using import()', () => {
+  it('Must check a string rendered by a chunk. Which is loaded via a click()', () => {
+    cy.visit('/')
 
-		cy.get('.code-splitting-btn')
-			.click()
+    cy.get('.code-splitting-btn').click()
 
-		cy.contains('This is some text within a chunk')
-	})
+    cy.contains('This is some text within a chunk')
+  })
 })
