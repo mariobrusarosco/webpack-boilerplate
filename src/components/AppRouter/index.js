5cc493968737c8783ba7ac0f01783996ef9b63f0
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-05-12 09:56:40 -0300

    Removing Infinite Loop

--- src/components/AppRouter/index.js
+++ src/components/AppRouter/index.js
@@ -1,15 +1,21 @@
 // Vendors
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 // Components
-import PageOne from 'pages/PageOne'
-import PageTwo from 'pages/PageTwo'
+import Header from 'components/Header'
+// Lazy Components
+const PageOne = lazy(() => import('pages/PageOne'))
+const PageTwo = lazy(() => import('pages/PageTwo'))
 
 const AppRouter = () => (
-  <BrowserRouter>
-    <Route path="/one" exact component={PageOne} />
-    <Route path="/two" exact component={PageTwo} />
-  </BrowserRouter>
+  <Suspense fallback={<div>...Loading...</div>}>
+    <BrowserRouter>
+      <Header />
+      <Route path="/one" exact component={PageOne} />
+      <Route path="/two" exact component={PageTwo} />
+    </BrowserRouter>
+  </Suspense>
 )
 
 export default AppRouter
