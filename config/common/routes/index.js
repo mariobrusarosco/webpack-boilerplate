49ed1b13dbdbb769cf3f08e6660203446cce64e8
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-08-30 11:51:02 -0300

    Draft version of route transitions without Suspense

--- config/common/routes/index.js
+++ config/common/routes/index.js
@@ -6,43 +6,45 @@ const { ROOT_URL } = APP
 
 // Pages
 import Home from 'pages/Home'
+import NotFound from 'pages/NotFound'
+import LosPollos from 'pages/LosPollos'
 
 const routes = [
   {
     name: 'Home',
     path: `${ROOT_URL}`,
-    component: Home,
+    Component: Home,
     exact: true
   },
   {
     name: 'Los Pollos Hermanos',
     path: `${ROOT_URL}los-pollos-hermanos`,
-    component: lazy(() => import('pages/LosPollos'))
+    Component: LosPollos
   },
-  {
-    name: 'Car Wash',
-    path: `${ROOT_URL}car-wash`,
-    component: lazy(() => import('pages/CarWash'))
-  },
-  {
-    name: 'Member',
-    path: `${ROOT_URL}member/:id`,
-    component: lazy(() => import('pages/Member'))
-  },
-  {
-    name: 'Product',
-    path: `${ROOT_URL}product/:id`,
-    component: lazy(() => import('pages/Product'))
-  },
-  {
-    name: 'Ops',
-    path: `${ROOT_URL}ops`,
-    component: lazy(() => import('pages/AppError'))
-  },
+  // {
+  //   name: 'Car Wash',
+  //   path: `${ROOT_URL}car-wash`,
+  //   Component: 'pages/CarWash'
+  // },
+  // {
+  //   name: 'Member',
+  //   path: `${ROOT_URL}member/:id`,
+  //   Component: 'pages/Member'
+  // },
+  // {
+  //   name: 'Product',
+  //   path: `${ROOT_URL}product/:id`,
+  //   Component: 'pages/Product'
+  // },
+  // {
+  //   name: 'Ops',
+  //   path: `${ROOT_URL}ops`,
+  //   Component: 'pages/AppError'
+  // },
   {
     name: 'NotFound',
     path: `*`,
-    component: lazy(() => import('pages/NotFound'))
+    Component: NotFound
   }
 ]
 
--- src/components/AppRouter/index.js
+++ src/components/AppRouter/index.js
@@ -2,6 +2,7 @@
 import { Suspense } from 'react'
 import { useSelector } from 'react-redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 // Routes Data
 import routes from 'configPath/common/routes'
@@ -12,20 +13,72 @@ import AppBootstrap from 'components/AppBootstrap'
 import AppLayout from 'components/AppLayout'
 import RoutesLoader from 'components/Loaders/RoutesLoader'
 
+import Home from 'pages/Home'
+import NotFound from 'pages/NotFound'
+import LosPollos from 'pages/LosPollos'
+
+// const Home = () => <div className="home">HOme</div>
+// const NotFound = () => <div className="not-found">not found</div>
+// const LosPollos = () => <div className="los-pollos">los pollos</div>
+
 const RouterStructure = () => {
   return (
     <BrowserRouter>
-      <Header />
+      <Route
+        render={({ location }) => (
+          <>
+            <Header />
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                classNames="route-motion"
+                timeout={300}
+                appear
+              >
+                <div>
+                  <AppLayout>
+                    <Switch location={location}>
+                      <Route exact path="/" component={Home} />
+                      <Route exact path="/los-pollos-hermanos" component={LosPollos} />
+                      <Route path="*" component={NotFound} />
+                    </Switch>
+                  </AppLayout>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
+          </>
+        )}
+      />
+    </BrowserRouter>
+  )
+}
 
-      <AppLayout>
-        <Suspense fallback={<RoutesLoader />}>
-          <Switch>
-            {routes.map((props, i) => (
-              <Route {...props} key={i} />
-            ))}
-          </Switch>
-        </Suspense>
-      </AppLayout>
+const RouterStructure2 = () => {
+  return (
+    <BrowserRouter>
+      {/* <Suspense fallback={<RoutesLoader />}>
+  <Switch>
+    {routes.map(({ path, Component }, i) => (
+      <Route exact path={path} key={i}>
+        {({ match }) => {
+          console.log(match)
+          return (
+            <CSSTransition
+              in={match != null}
+              timeout={300}
+              classNames="page"
+              unmountOnExit
+            >
+              <div className="page">
+                <Component match={match} />
+              </div>
+            </CSSTransition>
+          )
+        }}
+      </Route>
+    ))}
+  </Switch>
+  </Suspense> */}
     </BrowserRouter>
   )
 }
