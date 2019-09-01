f1b5ebd7c5f08317b26c009dd45f8c1022b9806d
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-09-01 16:52:18 -0300

    fix: Manifest JSON works work on development

--- webpack/plugins/common/index.js
+++ webpack/plugins/common/index.js
@@ -17,45 +17,32 @@ const commonPlugins = () => [
     favicon: resolve('src/assets/images/icon.png')
   }),
   new FaviconsWebpackPlugin({
-    logo: resolve('src/assets/images/icon.png'),
     mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
     devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
+    logo: resolve('src/assets/images/icon.png'),
+    cache: true,
+    inject: true,
     favicons: {
-      appName: 'my-app',
-      appDescription: 'My awesome App',
-      developerName: 'Me',
-      developerURL: null, // prevent retrieving from the nearest package.json
-      background: '#ddd',
-      theme_color: '#333',
+      appName: 'Boilerplate',
+      appShortName: 'Front End Boilerplate',
+      appDescription: 'A Boilerplate for Front End Development - Mario Brusarosco',
+      start_url: '/index.html?origin=pwa',
+      scope: '/',
+      display: 'standalone',
+      orientation: 'any', // 'landscape' or 'portrait' doesn't allow switch rotation... also 'portrait-primary' doesn't allow 180deg switch orientation
+      theme_color: '#00ac95',
+      background: '#c8d419',
+      appleStatusBarStyle: '#00ac95',
+      dir: 'auto',
+      lang: 'pt-BR',
+      crossorigin: null,
       icons: {
-        coast: false,
-        yandex: false
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        favicons: true
       }
     }
-    // logo: path.resolve('src/assets/images/icon.png'),
-    // cache: true,
-    // inject: true,
-    // favicons: {
-    //   appName: 'Boilerplate',
-    //   appShortName: 'Front End Boilerplate',
-    //   appDescription: 'A Boilerplate for Front End Development - Mario Brusarosco',
-    //   start_url: '/index.html?origin=pwa',
-    //   scope: '/',
-    //   display: 'standalone',
-    //   orientation: 'any', // 'landscape' or 'portrait' doesn't allow switch rotation... also 'portrait-primary' doesn't allow 180deg switch orientation
-    //   theme_color: '#00ac95',
-    //   background: '#c8d419',
-    //   appleStatusBarStyle: '#00ac95',
-    //   dir: 'auto',
-    //   lang: 'pt-BR',
-    //   crossorigin: null,
-    //   icons: {
-    //     android: true,
-    //     appleIcon: true,
-    //     appleStartup: true,
-    //     favicons: true
-    //   }
-    // }
   }),
   new CleanWebpack(['dist'], {
     root: process.cwd()
@@ -67,27 +54,6 @@ const commonPlugins = () => [
   new webpack.DefinePlugin({
     APP: JSON.stringify(config)
   }),
-  // new WebpackPwaManifest({
-  //   inject: true,
-  //   name: 'Boilerplate',
-  //   short_name: 'Front End Boilerplate',
-  //   start_url: '/index.html?origin=pwa',
-  //   scope: '.',  // '.' means all files inside the 'src' folder
-  //   display: 'standalone',
-  //   orientation: 'any',  // 'landscape' or 'portrait' doesn't allow switch rotation... also 'portrait-primary' doesn't allow 180deg switch orientation
-  //   theme_color: '#00ac95',
-  //   description: 'A Boilerplate for Front End Development - Mario Brusarosco',
-  //   background_color: '#c8d419',
-  //   dir: 'ltr',
-  //   lang: 'pt-BR',
-  //   crossorigin: null,
-  //   icons: [
-  //     {
-  //       src: resolve('src/assets/images/icon.png'),
-  //       sizes: [72, 96, 128, 144, 152, 192, 384, 512] // multiple sizes
-  //     }
-  //   ]
-  // }),
   new InjectManifest({
     swSrc: './src/sw.js',
     swDest: 'sw.js',
