83c92371ffee96838decbe69d8a5af19105be176
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-09-03 14:05:56 -0300

    feat: Basic Styles for buttons

--- src/components/GoBack/index.js
+++ src/components/GoBack/index.js
@@ -1,12 +1,21 @@
 // Vendors
 import { memo } from 'react'
 import classnames from 'classnames'
+
+// Styles
+import css from './styles.scss'
+
 // Utils
 import history from 'utils/app-history'
 
 const GoBack = memo(({ customClass, buttonText = 'back' }) => {
+  const handleGoBack = () => history.goBack()
+
   return (
-    <button className="button go-back" onClick={() => history.goBack()}>
+    <button
+      className={classnames([css.btn], [customClass], 'button go-back')}
+      onClick={handleGoBack}
+    >
       {buttonText}
     </button>
   )
