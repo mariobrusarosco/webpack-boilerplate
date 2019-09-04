db3efaf1c9903f881853cffcc3cb7f6beca7e9b5
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-09-04 16:47:32 -0300

    test: another layout

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -5,11 +5,13 @@ import {
   SwitchTransition,
   FadeTransition
 } from 'react-transition-group'
+import Masonry from 'react-masonry-component'
 
 import { memo } from 'react'
 
 // Components
 import ItemCard from 'components/ItemCard'
+import LazyImage from 'components/LazyImage'
 
 // Styles
 import css from './styles.scss'
@@ -45,4 +47,40 @@ const ItemsList = memo(({ items }) => {
   )
 })
 
-export default ItemsList
+const ItemsList2 = ({ items = [] }) => {
+  console.log(items)
+
+  // if (!items.length) return null
+
+  const childElements = items.map(function(element, i) {
+    return (
+      <li className={css.mansoryItem} key={element.id}>
+        {/* <img src={element.urls.small} /> */}
+        <LazyImage src={element.urls.small} />
+      </li>
+    )
+  })
+
+  const masonryOptions = {
+    transitionDuration: 0
+  }
+
+  const imagesLoadedOptions = { background: '.my-bg-image-el' }
+
+  return (
+    <div className={css.list}>
+      <Masonry
+        className={'my-gallery-class'} // default ''
+        elementType={'ul'} // default 'div'
+        options={masonryOptions} // default {}
+        disableImagesLoaded={false} // default false
+        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+        imagesLoadedOptions={imagesLoadedOptions} // default {}
+      >
+        {childElements}
+      </Masonry>
+    </div>
+  )
+}
+
+export default ItemsList2
