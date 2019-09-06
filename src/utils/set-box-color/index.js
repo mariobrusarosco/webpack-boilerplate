1920afbbe7ed33c473243995a671c99673e6ad34
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-09-06 10:20:13 -0300

    fix: Overflow

--- src/utils/set-box-color/index.js
+++ src/utils/set-box-color/index.js
@@ -4,11 +4,11 @@ const setBoxColor = async (img, box) => {
   const vib = new Vibrant(img)
   const swatches = await vib.getPalette()
 
-  const vibrant = swatches['Vibrant'].getRgb()
   const lightVibrant = swatches['LightVibrant'].getRgb()
-  const darkVibrant = swatches['DarkVibrant'].getRgb()
   const muted = swatches['Muted'].getRgb()
   const darkMuted = swatches['DarkMuted'].getRgb()
+  const vibrant = swatches['Vibrant'].getRgb()
+  const darkVibrant = swatches['DarkVibrant'].getRgb()
   const lightMuted = swatches['LightMuted'].getRgb()
 
   const backgroundRule = `linear-gradient(to right, rgb(${lightVibrant}), rgb(${muted}), rgb(${darkMuted}))`
